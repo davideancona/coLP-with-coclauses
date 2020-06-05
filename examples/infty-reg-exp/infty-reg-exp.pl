468a1734478649056328b8cc4d3e0a4e337ea718
Francesco Dagnino <fra.dagn@gmail.com>
2020-06-05 11:12:09 +0200

    fix infty-reg-exp.pl

--- examples/infty-reg-exp/infty-reg-exp.pl
+++ examples/infty-reg-exp/infty-reg-exp.pl
@@ -8,20 +8,23 @@ concat([X|W1],W2,[X|W]) :- concat(W1,W2,W).
 match([],eps).
 match([0],0).
 match([1],1).
-match(W,plus(R1,_)) :- match(W,R1).
-match(W,plus(_,R2)) :- match(W,R2).
-match(W,cat(R1,R2))  :- match(W1,R1), concat(W1,W2,W), match(W2,R2). 
-match(W,star(R))     :- match_star(N,W,R). 
-match(W,omega(R))    :- match(W1,R), concat(W1,W2,W); match(W2,omega(R)). 
-%%% match_star(N,W,R): W can be decomposed in N parts all matching R
+match(W,plus(R1,_))   :- match(W,R1).
+match(W,plus(_,R2))   :- match(W,R2).
+match(W,cat(R1,R2))   :- match(W1,R1), concat(W1,W2,W), match(W2,R2). 
+match(W,star(R))      :- match_star(N,W,R). 
+match([],omega(R))    :- match([],R). 
+match([B|W],omega(R)) :- match([B|W1],R), concat(W1,W2,W), match(W2,omega(R)). 
+%%% match_star(N,W,R) : W can be decomposed in N parts all matching R
 match_star(0,[],_). 
-match_star(s(N),W,R) :- match(W1,R), concat(W1,W2,W), match_star(N,W2,R). 
+match_star(s(N),W,R)  :- match(W1,R), concat(W1,W2,W), match_star(N,W2,R). 
 
 co(concat(W,_,W)).       %% cofact 
-co(match([],omega(R))).  %% cofact
-co(match(W,omega(R))) :- match([B|W1],R), concat([B|W1],W2,W). 
+%co(match([],omega(R))).  %% cofact
+%co(match(W,omega(R))) :- match([B|W1],R), concat([B|W1],W2,W). 
+co(match(W,omega(R))). 
 
 %%% tests
+%%% commented tests fail, because the interpreter is not complete
 main :-
     test_true01,
 %    test_true02,
@@ -30,12 +33,13 @@ main :-
 %    test_true05,
     test_true06,
     test_false01,
-%    test_false02,
+    test_false02,
     test_false03,
-%    test_false04,
+    test_false04,
     test_false05,
-%    test_false06,
-%    test_false07,
+    test_false06,
+    test_false07,
+    test_false08, 
     !,
     write('All tests passed').
 
@@ -69,15 +73,17 @@ test_true05 :- W01=[0,1|W01], solve(match(W01,omega(cat(star(0),1)))).
 
 test_true06 :- W0=[0|W0], solve(match(W0,cat(star(omega(cat(star(0),1) ) ) ,omega(0)))). 
 
-test_false05 :- W0=[0|W0], solve(match([1,1,0,1|W0],cat(star(1),omega(0)))).
+test_false05 :- W0=[0|W0], solve(match([1,1,0,1|W0],cat(star(1),omega(0)))),!,fail. 
 test_false05.
 
-test_false06 :- W01=[0,1|W01], solve(match(W01,star(cat(omega(plus(eps,0)),1)))). %% counter-example for initially incorrect version
+test_false06 :- W01=[0,1|W01], solve(match(W01,star(cat(omega(plus(eps,0)),1)))),!,fail.  %% counter-example for initially incorrect version
 test_false06.
 
-test_false07 :- W0=[0|W0], solve(match(W0,omega(cat(star(0),1)))).
+test_false07 :- W0=[0|W0], solve(match(W0,omega(cat(star(0),1)))),!,fail. 
 test_false07.
 
+test_false08 :- W0=[0|W0], solve(match([1|W0],omega(star(1)))),!,fail.
+test_false08.
 
 
 
